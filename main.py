b3cb966504ec0f26355a252353efda57881bb0f1
Skye Brown <skye.brownh@gmail.com>
2025-06-02 18:30:34 -0400

    pair with input device

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import asyncio
-from pyatv import scan
+from pyatv import scan, pair
+from pyatv.const import Protocol
 
 loop = asyncio.get_event_loop()
 
 async def atv_controller():
+    # scan for devices
     atvs = await scan(loop)
-    for atv in atvs:
-        print(f"Name: {atv.name}, Address: {atv.address}")
+    for index, atv in enumerate(atvs):
+        print(f"{index} Name: {atv.name}, Address: {atv.address}")
+        for service in atv.services:
+            print(f"{service}")
+        print("\n")
+
+    # pair from device selection
+    apple_tv = int(input("Which apple tv do you want to pair (index): "))
+    
+    pairing = await pair(atvs[apple_tv], Protocol.AirPlay, loop)
+    await pairing.begin()
+
+    pin = int(input("Pin: "))
+    pairing.pin(pin)
+    await pairing.finish()
+
+    # give feedback on the process
+    if pairing.has_paired:
+        print("Paired with device!")
+        print("Credentials: ", pairing.service.credentials)
+    else:
+        print("Did not pair with device")
+
+    await pairing.close()
 
 async def main():
     await atv_controller()
