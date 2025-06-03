2c05bfb639f2b98d6bed5c7d05f5a01e5f3c0e7f
Skye Brown <skye.brownh@gmail.com>
2025-06-03 06:27:22 -0400

    define MenuAction constant class, rework main.py into classes AppleTVMenuController and AppleTVDevice

--- core/storage.py
+++ core/storage.py
@@ -1,11 +1,6 @@
-# storage.py 
-# 
-# define and load storage
-
-from asyncio import AbstractEventLoop
 from pyatv.storage.file_storage import FileStorage
 
-async def load_storage(LOOP: AbstractEventLoop) -> FileStorage:
-    storage: FileStorage = FileStorage.default_storage(LOOP)
+async def load_storage(LOOP) -> FileStorage:
+    storage = FileStorage.default_storage(LOOP)
     await storage.load()
     return storage
--- main.py
+++ main.py
@@ -1,69 +1,21 @@
 import asyncio
 
-from pyatv import scan, pair, connect
-from pyatv.const import Protocol
-
 from core.storage import load_storage
-from core.menu import main_menu, handle_action
+from core.apple_tv_device import AppleTVDevice
+from core.menu_controller import AppleTVMenuController
 
-TARGET_NAME = "Apt Alternate ATV"
 LOOP = asyncio.get_event_loop()
 
 async def main():
     storage = await load_storage(LOOP)
 
-    # scan and match by name
-    atvs = await scan(LOOP, protocol=Protocol.AirPlay, storage=storage)
-    selected = next((d for d in atvs if d.name == TARGET_NAME), None)
+    device = AppleTVDevice('Apt Alternate ATV', storage)
+    await device.connect(LOOP)
 
-    # if not found, scan and pair
-    if not selected:
-        print(f"Device '{TARGET_NAME}' not found.")
-        print("Finding other devices to pair...")
-        selected = await pair_another_device(atvs, storage)
+    controller = AppleTVMenuController(device)
+    await controller.run()
 
-    # connect to paired device
-    device = await connect(selected, LOOP, storage=storage)
-
-    # perform actions
-    remote = device.remote_control
-    
-    while True:
-        action = main_menu()
-        if action == "exit":
-            break
-        await handle_action(action, remote, device)
-
-    # close connection
-    device.close()
-
-async def pair_another_device(atvs, storage):
-    # loop through devices
-    for index, atv in enumerate(atvs):
-        print(f"{index} Name: {atv.name}, Address: {atv.address}")
-        for service in atv.services:
-            print(f"{service}")
-        print("\n")
-
-    # pair from device selection
-    atv_input = int(input("Which apple tv do you want to pair (index): "))
-    
-    pairing = await pair(atvs[atv_input], Protocol.AirPlay, LOOP, storage=storage)
-    await pairing.begin()
-
-    pin = int(input("Pin: "))
-    pairing.pin(pin)
-    await pairing.finish()
-
-    # give feedback on the process
-    if pairing.has_paired:
-        print("Paired with device!")
-        print("Credentials: ", pairing.service.credentials)
-    else:
-        print("Did not pair with device")
-
-    await pairing.close()
-    return atvs[atv_input]
+    await device.disconnect()
 
 if __name__ == '__main__':
     LOOP.run_until_complete(main())
